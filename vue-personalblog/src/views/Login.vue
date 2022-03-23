50d37fa4e56ba5d9e83bfe25c39382a7a26aa8fc
smile159 <1174139226@qq.com>
2022-03-23 21:59:20 +0800

    添加注册页面以及登录和注册的切换动画

--- vue-personalblog/src/views/Login.vue
+++ vue-personalblog/src/views/Login.vue
@@ -2,11 +2,32 @@
   <div id="bj">
     <div class="login-box">
       <el-card>
-        <h2 class="login-title">登录</h2>
-        <!-- 手机号/用户名/邮箱/密码-->
-        <div class="name_pass">
+        <!-- 手机号/用户名/邮箱/密码  登录界面-->
+          <div :class="loginClass" ref="loginDiv">
+            <h2 class="title">登录</h2>
+            <el-input
+                placeholder="请输入手机号 / 用户名 / 邮箱"
+                prefix-icon="el-icon-user"
+                v-model="loginData.u">
+            </el-input>
+            <el-input
+                placeholder="请输入密码"
+                prefix-icon="el-icon-lollipop"
+                v-model="loginData.p">
+            </el-input>
+            <div>
+              <el-checkbox v-model="remember" label="1" class="remember-user">记住我</el-checkbox>
+              <a href="#" class="lost-pass">忘记密码？</a>
+            </div>
+            <el-button round class="loginBtn" @click="login">芝麻开门</el-button>
+            <div class="goRegister" @click="moveRegisterDiv">没有账号？立即注册</div>
+          </div>
+
+      <!-- 注册界面 -->
+        <div :class="registerClass">
+          <h2 class="title">注册</h2>
           <el-input
-              placeholder="请输入手机号 / 用户名 / 邮箱"
+              placeholder="请输入用户名"
               prefix-icon="el-icon-user"
               v-model="loginData.u">
           </el-input>
@@ -15,12 +36,15 @@
               prefix-icon="el-icon-lollipop"
               v-model="loginData.p">
           </el-input>
-          <div>
-            <el-checkbox v-model="remember" label="1" class="remember-user">记住我</el-checkbox>
-            <a href="#" class="lost-pass">忘记密码？</a>
-          </div>
+          <el-input
+              placeholder="请确认密码"
+              prefix-icon="el-icon-lollipop"
+              v-model="loginData.p">
+          </el-input>
           <el-button round class="loginBtn" @click="login">芝麻开门</el-button>
+          <div class="goLogin" @click="moveLoginDiv">没有账号？立即注册</div>
         </div>
+
       </el-card>
     </div>
   </div>
@@ -39,7 +63,9 @@ export default {
         u: "",
         p: ""
       },
-      remember: false
+      remember: false,
+      loginClass:['name_pass'],
+      registerClass:['register']
     }
   },
   methods: {
@@ -67,6 +93,17 @@ export default {
       },err =>{
         console.log("err = ",err)
       })
+    },
+    moveRegisterDiv(){
+      this.loginClass.push("switch_register")
+      // 删除switch_login
+      this.loginClass = this.loginClass.filter( item =>{
+        return item !== "switch_login";
+      })
+    },
+    moveLoginDiv(){
+      this.loginClass.pop();
+      this.loginClass.push("switch_login")
     }
   }
 }
@@ -89,16 +126,6 @@ export default {
   margin-top: -250px;
 }
 
-.login-box .login-title {
-  background: linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248));
-  /* 将文字区域剪裁出来 */
-  -webkit-background-clip: text;
-  /* 将文字颜色改为透明色 */
-  color: transparent;
-  text-align: center;
-  margin-top: 10%;
-}
-
 /*登录卡片*/
 .el-card {
   width: 500px;
@@ -107,22 +134,48 @@ export default {
 
 /*输入框的盒子*/
 .name_pass {
-  position: absolute;
-  top: 30%;
+  /*display: none;*/
   width: 460px;
+  height: 500px;
   padding: 0 20px;
+  transition: all 1s;
+  /*background-color: skyblue;*/
 }
 
-.name_pass .el-input {
-  padding: 5px 0;
+/*将页面内容切换到注册页面*/
+.switch_register {
+  /*触发的动画为moveRegister 用时1s 速度曲线为ease 在终点停下*/
+  animation: moveRegister 1s ease forwards;
 }
 
-.name_pass .loginBtn {
+/*将页面内容切换到登录页面*/
+.switch_login {
+  animation: moveLogin 1s ease forwards;
+}
+
+
+
+.el-card .title {
+  padding: 30px;
+  background: linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248));
+  /* 将文字区域剪裁出来 */
+  -webkit-background-clip: text;
+  /* 将文字颜色改为透明色 */
+  color: transparent;
+  text-align: center;
+}
+
+
+.el-card .el-input {
+  padding: 6px 0;
+}
+
+.el-card .loginBtn {
   margin-top: 30px;
   width: 460px;
 }
 
-.name_pass .loginBtn:hover,.loginBtn:active {
+.el-card .loginBtn:hover,.loginBtn:active {
   background: linear-gradient(to right top, rgb(254, 202, 202), rgb(252, 165, 165), rgb(254, 240, 138));
   color: #333333;
   border-color: transparent;
@@ -140,5 +193,44 @@ export default {
   text-decoration: none;
 }
 
+/*立即注册*/
+.name_pass .goRegister,.el-card .goLogin {
+  background: linear-gradient(to right, rgb(249, 168, 212), rgb(216, 180, 254), rgb(129, 140, 248));
+  /* 将文字区域剪裁出来 */
+  -webkit-background-clip: text;
+  /* 将文字颜色改为透明色 */
+  color: transparent;
+  text-align: center;
+  padding: 60px 0;
+}
+
+/*注册界面的父div*/
+.register {
+  width: 460px;
+  height: 500px;
+  /*background-color: saddlebrown;*/
+  margin: 0 auto;
+  transition: all 1s;
+}
+
+/*定义登录切换到注册的动画*/
+@keyframes moveRegister {
+  0% {
+    margin-top: 0;
+  }
+  100% {
+    margin-top: -500px;
+  }
+}
+
+/*定义从注册切换到登录的界面*/
+@keyframes moveLogin {
+  0% {
+    margin-top: -500px;
+  }
+  100% {
+    margin-top: 0;
+  }
+}
 
 </style>
